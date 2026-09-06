5a3d439477b1dc51cef251f08460ef6302fcaa62
twoten <twoten.eq@gmail.com>
2026-09-06 17:15:06 +0300

    1788704106

--- flake.nix
+++ flake.nix
@@ -1,29 +1,51 @@
 {
-  description = "InfiniTime";
+  outputs =
+    { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+    in
+    {
+      devShells = lib.genAttrs lib.systems.flakeExposed (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+              permittedInsecurePackages = [
+                "python3.14-ecdsa-0.19.2"
+              ];
+            };
+          };
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+          nrf5-sdk = pkgs.nrf5-sdk;
+          gcc-arm-embedded = pkgs.gcc-arm-embedded;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              nrf5-sdk
+              gcc-arm-embedded
+              pkgs.cmake
+              pkgs.ninja
+              pkgs.nodejs
+              pkgs.lv_font_conv
+              pkgs.SDL2
+              pkgs.adafruit-nrfutil
+              (pkgs.python3.withPackages (
+                pkgs: with pkgs; [
+                  cryptography
+                  intelhex
+                  click
+                  cbor
+                ]
+              ))
+            ];
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-
-        nodePackages = pkgs.nodePackages;
-
-      in {
-        devShell = pkgs.mkShell {
-          name = "infinitime-dev-shell";
-
-          buildInputs = with pkgs; [
-            cmake
-            ninja
-            nodejs
-            nodePackages.lv_font_conv
-            SDL2
-          ];
-        };
-      });
+            ARM_NONE_EABI_TOOLCHAIN_PATH = "${gcc-arm-embedded}";
+            NRF5_SDK_PATH = "${nrf5-sdk}/share/nRF5_SDK";
+          };
+        }
+      );
+    };
 }
